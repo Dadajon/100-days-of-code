77c910910e5c18afa0c19beecc46ed403dc3eefc
Dadajon <dadajonjurakuziev@gmail.com>
2019-10-18 03:40:02 +0900

    D1

---
+++ competitive-programming/hackerrank/30-days-of-code/day-1/data-types.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+int_var = int(input())
+float_var = float(input())
+str_var = input()
+
+print(i+int_var)
+print(d+float_var)
+print(s+str_var)
