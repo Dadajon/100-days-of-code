2b0e6c26fe647c727ff500b3b9bffaed48ee41be
Dadajon <dadajonjurakuziev@gmail.com>
2019-10-20 22:32:16 +0900

    Solved #divisible-sum-pairs problem

---
+++ competitive-programming/hackerrank/algorithms/16-divisible-sum-pairs/divisible-sum-pairs.py
@@ -0,0 +1,22 @@
+
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(len(ar)):
+        for j in range(i + 1, len(ar)):
+            if (ar[i] + ar[j]) % k == 0:
+                count += 1
+    return count
+
+
+if __name__ == '__main__':
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    print(str(result) + '\n')
