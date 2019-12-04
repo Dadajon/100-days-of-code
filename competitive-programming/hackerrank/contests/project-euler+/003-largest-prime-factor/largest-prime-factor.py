283c117940bb2ff9245a29beb45cf90ae91ccd81
Dadajon <dadajonjurakuziev@gmail.com>
2019-12-04 23:58:20 +0900

    Solved #largest-prime-factor problem

---
+++ competitive-programming/hackerrank/contests/project-euler+/003-largest-prime-factor/largest-prime-factor.py
@@ -0,0 +1,23 @@
+
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        N = number = int(input())
+        largest = 2
+        while not number % 2:
+            number //= 2
+        factor = 3
+        max_factor = number ** 0.5
+        while factor <= max_factor:
+            if not number % factor:
+                largest = factor
+                number //= factor
+                while not number % factor:
+                    number //= factor
+                if number == 1:
+                    print(largest)
+                    break
+                max_factor = number ** 0.5
+            factor += 2
+        else:
+            print(number)
